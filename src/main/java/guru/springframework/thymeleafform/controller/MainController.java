ef17a2342e83a0c3b116afa3b071d5eb24e1213b
kr-avinash <73075560+kr-avinash@users.noreply.github.com>
2021-11-24 21:38:31 +0530

    update

---
+++ src/main/java/guru/springframework/thymeleafform/controller/MainController.java
@@ -0,0 +1,33 @@
+package guru.springframework.thymeleafform.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import guru.springframework.thymeleafform.model.User;
+
+@Controller
+public class MainController {
+
+	@GetMapping("/register")
+	public String showForm(Model model) {
+		User user = new User();
+		model.addAttribute("user", user);
+		
+		List<String> listProfession = Arrays.asList("Developer", "Tester", "Architect", "QA");
+		model.addAttribute("listProfession", listProfession);
+		
+		return "register_form";
+	}
+	
+	@PostMapping("/register")
+	public String submitForm(@ModelAttribute("user") User user) {
+		System.out.println(user);
+		return "register_success";
+	}
+}
